e9ed008411d7fe0fc8e8e6ca596fef923b1d9dab
Olivier Scherrer <pode.fr@gmail.com>
2014-04-03 23:59:15 -0400

    add implementation + tests

--- index.js
+++ index.js
@@ -6,3 +6,78 @@
 * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com>
 */
 "use strict";
+
+var assert = require("assert");
+
+/**
+ * Get the closest number in an array
+ * @param {Number} item the base number
+ * @param {Array} array the array to search into
+ * @param {Function} getDiff returns the difference between the base number and
+ *   and the currently read item in the array. The item which returned the smallest difference wins.
+ * @private
+ */
+function _getClosest(item, array, getDiff) {
+    var closest,
+        diff;
+
+    assert(Array.isArray(array), "Get closest expects an array as second argument");
+
+    array.forEach(function (comparedItem, comparedItemIndex) {
+        var thisDiff = getDiff(comparedItem, item);
+
+        if (thisDiff >= 0 && (typeof diff == "undefined" || thisDiff < diff)) {
+            diff = thisDiff;
+            closest = comparedItemIndex;
+        }
+    });
+
+    return closest;
+}
+
+module.exports = {
+
+  custom: function closestCustom(item, array, comparator) {
+    return _getClosest(item, array, comparator);
+  },
+
+  /**
+   * Get the closest number in an array given a base number
+   * Example: closest(30, [20, 0, 50, 29]) will return 3 as 29 is the closest item
+   * @param {Number} item the base number
+   * @param {Array} array the array of numbers to search into
+   * @returns {Number} the index of the closest item in the array
+   */
+  number: function closestNumber(item, array) {
+      return _getClosest(item, array, function (comparedItem, item) {
+          return Math.abs(comparedItem - item);
+      });
+  },
+
+  /**
+   * Get the closest greater number in an array given a base number
+   * Example: closest(30, [20, 0, 50, 29]) will return 2 as 50 is the closest greater item
+   * @param {Number} item the base number
+   * @param {Array} array the array of numbers to search into
+   * @returns {Number} the index of the closest item in the array
+   */
+  greaterNumber: function closestGreaterNumber(item, array) {
+      return _getClosest(item, array, function (comparedItem, item) {
+          return comparedItem - item;
+      });
+  },
+
+  /**
+   * Get the closest lower number in an array given a base number
+   * Example: closest(30, [20, 0, 50, 29]) will return 0 as 20 is the closest lower item
+   * @param {Number} item the base number
+   * @param {Array} array the array of numbers to search into
+   * @returns {Number} the index of the closest item in the array
+   */
+  lowerNumber: function closestLowerNumber(item, array) {
+    return _getClosest(item, array, function (comparedItem, item) {
+        return item - comparedItem;
+    });
+  }
+
+};
--- test/get-closest.spec.js
+++ test/get-closest.spec.js
@@ -5,4 +5,86 @@
 *
 * Copyright (c) 2014 Olivier Scherrer <pode.fr@gmail.com
 */
-var sut = require("../index");
+var sut = require("../index"),
+	chai = require("chai"),
+	expect = chai.expect;
+
+describe("Closest", function () {
+
+	var array = [10, 15, 0, 20, 5];
+
+	it("should be a function", function () {
+		expect(typeof sut.number).to.equal("function");
+	});
+
+	it("should return the closest item in an array", function () {
+		expect(function () {
+			sut.number();
+		}).to.throw("Get closest expects an array as second argument");
+		expect(function () {
+			sut.number(0);
+		}).to.throw("Get closest expects an array as second argument");
+		expect(sut.number(0, array)).to.equal(2);
+		expect(sut.number(2, array)).to.equal(2);
+		expect(sut.number(3, array)).to.equal(4);
+		expect(sut.number(2.5, array)).to.equal(2);
+		expect(sut.number(9, array)).to.equal(0);
+		expect(sut.number(10, array)).to.equal(0);
+		expect(sut.number(15.5, array)).to.equal(1);
+		expect(sut.number(20, array)).to.equal(3);
+	});
+
+});
+
+describe("ClosestGreater", function () {
+
+	var array = [10, 15, 1, 20, 5];
+
+	it("should be a function", function () {
+		expect(typeof sut.greaterNumber).to.equal("function");
+	});
+
+	it("should return the closest greater or equal item in an array", function () {
+		expect(function () {
+			sut.greaterNumber();
+		}).to.throw("Get closest expects an array as second argument");
+		expect(function () {
+			sut.greaterNumber(0);
+		}).to.throw("Get closest expects an array as second argument");
+		expect(sut.greaterNumber(0, array)).to.equal(2);
+		expect(sut.greaterNumber(2, array)).to.equal(4);
+		expect(sut.greaterNumber(3, array)).to.equal(4);
+		expect(sut.greaterNumber(2.5, array)).to.equal(4);
+		expect(sut.greaterNumber(9, array)).to.equal(0);
+		expect(sut.greaterNumber(10, array)).to.equal(0);
+		expect(sut.greaterNumber(15.5, array)).to.equal(3);
+		expect(sut.greaterNumber(20, array)).to.equal(3);
+	});
+
+});
+
+describe("ClosestLower", function () {
+
+	var array = [10, 15, 0, 20, 5];
+
+	it("should be a function", function () {
+		expect(typeof sut.lowerNumber).to.equal("function");
+	});
+
+	it("should return the closest lower or equal item in an array", function () {
+		expect(function () {
+			sut.lowerNumber();
+		}).to.throw("Get closest expects an array as second argument");
+		expect(function () {
+			sut.lowerNumber(0);
+		}).to.throw("Get closest expects an array as second argument");
+		expect(sut.lowerNumber(0, array)).to.equal(2);
+		expect(sut.lowerNumber(4, array)).to.equal(2);
+		expect(sut.lowerNumber(9, array)).to.equal(4);
+		expect(sut.lowerNumber(10, array)).to.equal(0);
+		expect(sut.lowerNumber(14, array)).to.equal(0);
+		expect(sut.lowerNumber(15.5, array)).to.equal(1);
+		expect(sut.lowerNumber(20, array)).to.equal(3);
+	});
+
+});
